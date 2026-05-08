c3bf53a9a9122a9dff57245aa1ce3a2dc6143e40
gianna-longo <glongo@hawaii.edu>
2026-05-07 14:17:05 -1000

    adding smaller algorithm folder

---
+++ 476_Final/hls4ml_project_small/firmware/weights/b13.h
@@ -0,0 +1,15 @@
+//Numpy array shape [3]
+//Min -0.204016208649
+//Max 0.237544730306
+//Number of zeros 0
+
+#ifndef B13_H_
+#define B13_H_
+
+#ifndef __SYNTHESIS__
+dense_1_bias_t b13[3];
+#else
+dense_1_bias_t b13[3] = {-0.204016208648682, 0.237544730305672, -0.052897684276104};
+#endif
+
+#endif
---
+++ 476_Final/hls4ml_project_small/firmware/weights/b2.h
@@ -0,0 +1,15 @@
+//Numpy array shape [4]
+//Min -0.069587521255
+//Max 0.094608508050
+//Number of zeros 1
+
+#ifndef B2_H_
+#define B2_H_
+
+#ifndef __SYNTHESIS__
+conv1d_bias_t b2[4];
+#else
+conv1d_bias_t b2[4] = {0.094608508050442, -0.069587521255016, 0.074976585805416, 0.000000000000000};
+#endif
+
+#endif
